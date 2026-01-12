bf9390fbc8fb92f81587e50b51694bf993234cde
felix <ghfs102030@gmail.com>
2026-01-12 16:31:55 -0300

    feat: sending AI response through Whatsapp

--- src/main/java/com/gfelix/langchain4j/AssistantController.java
+++ src/main/java/com/gfelix/langchain4j/AssistantController.java
@@ -1,18 +1,34 @@
 package com.gfelix.langchain4j;
 
+import com.twilio.Twilio;
 import dev.langchain4j.service.Result;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.twilio.Twilio;
+import com.twilio.converter.Promoter;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+import java.net.URI;
+import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("/api/assistant")
 public class AssistantController {
 
     private final AssistantAiService assistantAiService;
+
+    @Value("${twilio.account.sid}")
+    private String accountSid;
+    @Value("${twilio.auth.token}")
+    private String twillioAuth;
+    @Value("${twilio.whatsapp.number}")
+    private String twillioWhatsappNumber;
 
     public AssistantController(AssistantAiService assistantAiService) {
         this.assistantAiService = assistantAiService;
@@ -28,16 +44,14 @@ public class AssistantController {
 
     @PostMapping("/receive")
     public ResponseEntity<String> receiveWhatsAppMessage(@RequestBody MultiValueMap<String, String> body) {
-        String from = body.getFirst("From");
+        String fromPhoneNumber = body.getFirst("From");
         String messageBody = body.getFirst("Body");
 
-        Result<String> result = assistantAiService.handleRequest(from, messageBody);
+        Result<String> result = assistantAiService.handleRequest(fromPhoneNumber, messageBody);
 
+        System.out.println("Received message: " + messageBody + " from " + fromPhoneNumber + ": " + result.content());
 
-        // Process the message here (e.g., log, save to DB)
-        System.out.println("Received message: " + messageBody + " from " + from);
-
-        return ResponseEntity.ok("Message received");
+        return ResponseEntity.ok(result.content());
     }
 
 }
